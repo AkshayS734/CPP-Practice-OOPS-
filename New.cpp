a27d301ecaaa9048336e0dde59c9615f64b2fb97
AkshayS734 <manasshukla876@gmail.com>
2023-02-23 22:11:42 +0530

    st

--- New.cpp
+++ New.cpp
@@ -1,5 +1,25 @@
 #include<iostream>
+#include<string>
 using namespace std;
+class questions{
+    private:
+    string reasons;
+    public:
+    void setwhy(){
+       cout<<"Enter the reason: ";
+       getline(cin,reasons);
+    }
+    void getwhy(){
+        cout<<"Reason was '"<<reasons<<"'"<<endl;
+    }
+};
+
 int main(){
-    
+    cout<<"Hello"<<endl;
+    questions person1;
+    person1.setwhy();
+    person1.getwhy();
+    cout<<"Thanks for using";
+    return 0;
+
 }
