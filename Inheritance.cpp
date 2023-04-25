95d05fd29c9b532055bea5095b2589c52d14bfd5
AkshayS734 <manasshukla876@gmail.com>
2023-04-25 22:10:34 +0530

    Inheritance added

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -1,2 +1,40 @@
 #include<iostream>
 using namespace std;
+class BaseClass{
+    protected:
+    int a,b,c;
+    public:
+    void marks(){
+        cout<<"Enter the marks of first subject : ";
+        cin>>a;
+        cout<<"Enter the marks of second subject : ";
+        cin>>b;
+        cout<<"Enter the marks of third subject : ";
+        cin>>c;
+    }
+};
+class DerivedClass:public BaseClass{
+    protected:
+    int tot;
+    public:
+    int total(){
+        tot=a+b+c;
+        return tot;
+    }
+};
+class SecondDerivedClass:public DerivedClass{
+    protected:
+    float avg;
+    public:
+    int average(){
+        avg=tot/3; 
+        return avg;
+    }
+};
+int main(){
+    SecondDerivedClass obj;
+    obj.marks();
+    cout<<"Total marks are "<<obj.total()<<endl;
+    cout<<"Average is "<<obj.average();
+    return 0;
+}
---
+++ PureVirtualFunction.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+class Parent{
+    public:
+    virtual void f1()=0;
+
+};
